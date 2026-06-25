61e2753fc9bd613ce3b90880c048fbc06f065b4c
demysti <demystiktech@gmail.com>
2026-06-25 17:06:54 +0100

    route configured

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,6 +1,8 @@
 // import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:washslot/features/auth/presentation/login_page.dart';
+import 'package:washslot/features/auth/presentation/reset_password_screen.dart';
+import 'package:washslot/features/auth/presentation/signup_screen.dart';
 import 'package:washslot/features/onboarding/presentation/first_onboarding.dart';
 import 'package:washslot/features/onboarding/presentation/second_onboarding.dart';
 
@@ -19,6 +21,15 @@ final GoRouter appRouter = GoRouter(
 
     GoRoute(path: "/login_screen",
     builder: (context, state) => const LoginPage(),
-    )
+    ),
+    
+    GoRoute(
+      path: "/signup_screen",
+      builder: (context, state) => SignupScreen(),
+    ),
+    GoRoute(path: "/reset_password_screen",
+    builder: (context, state) => const ResetPasswordScreen(),
+    ),
+
   ]
 );
